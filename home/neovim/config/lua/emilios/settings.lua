a70de3d598005d65cd68cc86fcffbc294f75868a
Emilio Srougo <emilios1995@gmail.com>
2023-06-25 20:00:20 -0600

    add basic vim settings

--- home/neovim/config/lua/emilios/settings.lua
+++ home/neovim/config/lua/emilios/settings.lua
@@ -1,4 +1,46 @@
-local opt = vim.opt
 local g = vim.g
+local keymap = vim.keymap
+local o = vim.o
 
-opt.guicursor = "i:block"
+-- show block cursor instead of blinking line on insert mode
+o.guicursor = "i:block"
+
+-- map space as leader key
+keymap.set('', '<Space>', '<Nop>', {noremap = true, silent = true})
+g.mapleader = ' '
+g.maplocalleader = ' '
+
+o.shiftwidth = 2 -- add 2 spaces at every indent level
+o.expandtab = true -- always convert tab to spaces
+
+o.relativenumber = true
+o.number = true
+
+
+-- incremental live completion
+o.inccommand = 'nosplit'
+
+-- don't highlight search results
+o.hlsearch = false 
+
+-- don't save when switching buffers
+o.hidden = true
+
+ -- enable mouse mode
+o.mouse = 'a'
+--
+ -- keep indent level when wrapping a line
+o.breakindent = true
+
+-- save undo history
+o.undofile = true
+
+-- ignore casing in search unless using /c or if the search contains an uppercase char
+o.ignorecase = true
+o.smartcase = true
+
+
+-- always show sign column (where numbers, folds, etc, are)
+signcolumn = 'yes'
+
+o.termguicolors = true
