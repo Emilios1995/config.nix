b2e683169780ce2c6e1a78127976c75e7567883b
Emilio Srougo <emilios1995@gmail.com>
2023-07-18 20:03:50 -0600

    add useful tmux binding for lazygit

--- home/tmux.nix
+++ home/tmux.nix
@@ -20,6 +20,7 @@
       set -as terminal-features ",xterm-256color:RGB"
       bind -T copy-mode-vi 'v' send -X begin-selection
       bind -T copy-mode-vi 'y' send -X copy-selection-and-cancel
+      bind-key G new-window -n lazygit -c "#{pane_current_path}" direnv exec . lazygit
     '';
   };
 }
