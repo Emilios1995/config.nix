85e55b335b94b8067c3e99c90dbbc9a82933f961
Emilio Srougo <emilios1995@gmail.com>
2024-02-17 20:57:09 -0600

    Add LSP for python

--- home/neovim/default.nix
+++ home/neovim/default.nix
@@ -60,6 +60,7 @@ with lib;
       nodePackages.typescript-language-server
       nodePackages."@tailwindcss/language-server"
       nodePackages.graphql-language-service-cli
+      nodePackages.pyright
       sg-nvim
     ]);
 
