792f17b0dfb4b66eeb6cfd885fe4e3aaeff2348c
Emilio Srougo <emilios1995@gmail.com>
2024-12-30 12:59:42 -0600

    Updates and fixes

--- home/tmux.nix
+++ home/tmux.nix
@@ -8,21 +8,22 @@
     keyMode = "vi";
     mouse = true;
 
-    plugins = with pkgs.tmuxPlugins; [
-      sensible
-      vim-tmux-navigator
-      pain-control
-      t-smart-tmux-session-manager
-      catppuccin
-    ];
+     plugins = with pkgs.tmuxPlugins; [
+       sensible
+       vim-tmux-navigator
+       pain-control
+       t-smart-tmux-session-manager
+       catppuccin
+     ];
 
-    extraConfig = ''
-      set -as terminal-features ",xterm-256color:RGB"
-      set -g @catppuccin_flavour 'latte'
-      bind -T copy-mode-vi 'v' send -X begin-selection
-      bind -T copy-mode-vi 'y' send -X copy-selection-and-cancel
-      bind-key G new-window -n lazygit -c "#{pane_current_path}" direnv exec . lazygit
-      bind-key A switch-client -l
-    '';
+     extraConfig = ''
+       set -as terminal-features ",xterm-256color:RGB"
+       set -g @catppuccin_flavour 'latte'
+       bind -T copy-mode-vi 'v' send -X begin-selection
+       bind -T copy-mode-vi 'y' send -X copy-selection-and-cancel
+       bind-key G new-window -n lazygit -c "#{pane_current_path}" direnv exec . lazygit
+       bind-key A switch-client -l
+       set -g default-command '$SHELL'
+     '';
   };
 }
