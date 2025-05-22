c3b7ed912f4bb0f97b9f53fa0b73a2e47f0447a9
Emilio Srougo <emilios1995@gmail.com>
2025-05-22 11:33:57 -0600

    LSP changes

--- home/neovim/config/lua/emilios/plugins/lsp.lua
+++ home/neovim/config/lua/emilios/plugins/lsp.lua
@@ -60,7 +60,16 @@ local servers = {
   pyright = mk_server(),
   ocamllsp = mk_server(),
   graphql = mk_server(),
-  hls = mk_server(),
+  hls = mk_server(
+    {
+      cmd = { "haskell-language-server", "--lsp" },
+      settings = {
+        haskell = {
+          formattingProvider = "fourmolu"
+        }
+      }
+    }
+  ),
   nil_ls = mk_server(),
   rescriptls = mk_server {
     cmd = { "npx", "@rescript/language-server@1.50.0", "--stdio" },
@@ -71,17 +80,7 @@ local servers = {
       }
     },
   },
-  tailwindcss = mk_server {
-    settings = {
-      tailwindCSS = {
-        experimental = {
-          classRegex = {
-            { "[\"'`]([^\"'`]*).*?[\"'`]" },
-          },
-        },
-      },
-    },
-  },
+  tailwindcss = mk_server(),
   lua_ls = mk_server {
     settings = {
       Lua = {
@@ -106,6 +105,9 @@ local servers = {
   },
   rescript_relay_lsp = mk_server(),
   purescriptls = mk_server(),
+  vtsls = mk_server {
+    cmd = { "npx", "@vtsls/language-server", "--stdio" }
+  }
 }
 
 
@@ -113,51 +115,3 @@ local servers = {
 for server, config in pairs(servers) do
   nvim_lsp[server].setup(config)
 end
-
-
-require("typescript-tools").setup {
-  on_attach = default_on_attach,
-  settings = {
-    -- spawn additional tsserver instance to calculate diagnostics on it
-    separate_diagnostic_server = true,
-    -- "change"|"insert_leave" determine when the client asks the server about diagnostic
-    publish_diagnostic_on = "insert_leave",
-    -- array of strings("fix_all"|"add_missing_imports"|"remove_unused"|
-    -- "remove_unused_imports"|"organize_imports") -- or string "all"
-    -- to include all supported code actions
-    -- specify commands exposed as code_actions
-    expose_as_code_action = {},
-    -- string|nil - specify a custom path to `tsserver.js` file, if this is nil or file under path
-    -- not exists then standard path resolution strategy is applied
-    tsserver_path = nil,
-    -- specify a list of plugins to load by tsserver, e.g., for support `styled-components`
-    -- (see 💅 `styled-components` support section)
-    tsserver_plugins = {},
-    -- this value is passed to: https://nodejs.org/api/cli.html#--max-old-space-sizesize-in-megabytes
-    -- memory limit in megabytes or "auto"(basically no limit)
-    tsserver_max_memory = "auto",
-    -- described below
-    tsserver_format_options = {},
-    tsserver_file_preferences = {},
-    -- locale of all tsserver messages, supported locales you can find here:
-    -- https://github.com/microsoft/TypeScript/blob/3c221fc086be52b19801f6e8d82596d04607ede6/src/compiler/utilitiesPublic.ts#L620
-    tsserver_locale = "en",
-    -- mirror of VSCode's `typescript.suggest.completeFunctionCalls`
-    complete_function_calls = false,
-    include_completions_with_insert_text = true,
-    -- CodeLens
-    -- WARNING: Experimental feature also in VSCode, because it might hit performance of server.
-    -- possible values: ("off"|"all"|"implementations_only"|"references_only")
-    code_lens = "off",
-    -- by default code lenses are displayed on all referencable values and for some of you it can
-    -- be too much this option reduce count of them by removing member references from lenses
-    disable_member_code_lens = true,
-    -- JSXCloseTag
-    -- WARNING: it is disabled by default (maybe you configuration or distro already uses nvim-ts-autotag,
-    -- that maybe have a conflict if enable this feature. )
-    jsx_close_tag = {
-      enable = false,
-      filetypes = { "javascriptreact", "typescriptreact" },
-    }
-  },
-}
