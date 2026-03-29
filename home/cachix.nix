0bdf1d30adbccc10fdc6683daea78fde110f1288
Emilio Srougo <emilios1995@gmail.com>
2026-03-29 12:22:38 -0600

    Configure cachix

--- home/cachix.nix
+++ home/cachix.nix
@@ -2,6 +2,12 @@
 {
   home.packages = [ pkgs.cachix ];
 
+  nix.settings = {
+    extra-substituters = [ "https://tan.cachix.org" ];
+    extra-trusted-public-keys = [ "tan.cachix.org-1:y9VYkIo4aZD4oK1wM/mYppPK0Pt//FMmTIyPcT6sbcs=" ];
+    netrc-file = age.secrets.nix-netrc.path;
+  };
+   
   xdg.configFile."cachix/cachix.dhall".text = ''
     { authToken = ${age.secrets.cachix-authtoken.path}
     , hostname = "https://cachix.org"
